a90f758dadc54d0efff9aeae3b5fbc31ae84ab0b
levisc8 <levisc8@gmail.com>
2018-03-07 17:54:22 +0100

    Update data base creation script and move user facing materials to RPadrino

--- data-raw/Text_Based_DB.R
+++ data-raw/Text_Based_DB.R
@@ -52,9 +52,9 @@ create_metadata_table_command <- "CREATE TABLE metadata
   demog_appendix_link character varying,
   duration integer NOT NULL,
   start_year integer NOT NULL,
-  start_month integer NOT NULL,
+  start_month integer,
   end_year integer NOT NULL,
-  end_month integer NOT NULL,
+  end_month integer,
   periodicity decimal (7, 4) NOT NULL,
   number_populations integer NOT NULL,
   lat decimal (15, 12),
@@ -121,7 +121,9 @@ create_model_statevar_table_command <- "CREATE TABLE state_variables
   state_variable character varying NOT NULL,
   lower real NOT NULL,
   upper real NOT NULL,
-  n_meshpoints smallint NOT NULL,
+  n_meshpoints integer NOT NULL,
+  discrete boolean NOT NULL,
+  discrete_type character varying,
   CONSTRAINT state_variables_pkey PRIMARY KEY (ipm_id, state_variable)
 )
 WITH (
@@ -133,121 +135,46 @@ OWNER TO postgres;
   
 dbGetQuery(con, create_model_statevar_table_command)
   
-## NOTE: CODE BELOW IS NOT TESTED AND DOES NOT ACTUALLY WORK
-## PROVIDED TO GIVE AN IDEA OF WHAT THE WORKFLOW WOULD 
-## LOOK LIKE 
-
 ## ---------------------------------------------------------- ##
 
 # Create tables for pushing to SQL 
 # First option - enter data in excel sheets, convert to csv, read
-# into R and then prepare for sending to SQL.
+# into R and then prepare for sending to SQL. This seems like
+# best idea to retain similarity with COM(P)ADRE workflows
 
-Model <- list(Metadata = read.csv('data-raw/metadata.csv',
-                                  stringsAsFactors = FALSE),
-              StateVar = read.csv('data-raw/state_vars.csv',
-                                  stringsAsFactors = FALSE),
-              ModelExp = read.csv('data-raw/model_exprs.csv',
-                                  stringsAsFactors = FALSE),
-              ModelVal = read.csv('data-raw/model_values.csv',
-                                  stringsAsFactors = FALSE))
-
-# OR - Create a sample model representing the expressions to build an
-# IPM using a list in R. This is definitely more reproducible,
-# but comes at the expense of requiring a bit more tech savvy. 
-Model <- list(Parameters = list(Growth = list(Formula = "sizeNext ~ Normal(mu, sd)",
-                                              mu = "2.751 + 0.407 * Size",
-                                              sd = "sqrt(9.119 * exp(-0.228 * Size))",
-                                              Model = c("GLM", "Gaussian")),
-                                Survival = list(Formula = "logit(s_z) ~ SurvInt + SurvSlope * Size",
-                                                SurvInt = -2.27,
-                                                SurvSlope = 0.569,
-                                                Model = c("GLM", "Logistic")),
-                                Fecundity = list(FlowerP = list(Formula = "logit(f_z) ~ FlowerPInt +
-                                                                                  FlowerPSlope * Size",
-                                                                FlowerPInt = -2.107,
-                                                                FlowerPSlope = 0.86,
-                                                                Model = c("GLM", "Logistic")),
-                                                 FlowerN = list(Formula = "FlowerN ~ FlowerNInt + 
-                                                                                FlowerNSlope * exp(Size)",
-                                                              FlowerNInt = 6.363,
-                                                              FlowerNSlope = 0.0056,
-                                                              Model = c("GLM", "Poisson")),
-                                                 Seeds = list(Formula = "Constant",
-                                                              Constant = 374,
-                                                              Model = c("", "Constant")),
-                                                 EstProb = list(Formula = "Constant",
-                                                                Constant = "0.019",
-                                                                Model = c("", "Constant")),
-                                                 RecrSize = list(Formula = "SizeNext ~ Normal(mu, sd)",
-                                                                 mu = -0.771,
-                                                                 sd = 1.719,
-                                                                 Model = c("PDensity", "Gaussian")),
-                                                 EnterDiscrete = list(Formula = "Constant",
-                                                                      Constant = 0.157,
-                                                                      Model = c("", "Constant")),
-                                                 StayDiscrete = list(Formula = "Constant",
-                                                                     Constant = 0.038,
-                                                                     Model = c("", "Constant")),
-                                                 LeaveDiscrete = list(Formula = "Constant",
-                                                                      Constant = 0.1847,
-                                                                      Model = c("", "Constant")))),
-              Metadata = data.frame(species_name = "Carduus_nutans",
-                                    ipm_id = "A11111",
-                                    author = c('Jongejans E; Shea K; Skarpas O; Kelly D; Ellner S'),
-                                    doi = "10.1890/09-2226.1"))
-
-# Eelke did not include the upper and lower size bounds of his populations (!!!!)
-# but I do have the real boundaries from the ipmBase3_EJ.R script. Future
-# Padrino digitizers will not have that luxury. Emailing the author is probably the best
-# option here.
-Metadata <- Model$Metadata
-ModelExpressions <- data.frame(ipm_id = rep("A11111", 4),
-                               demographic_parameter = c("Survival", "Growth", "PFlowering", "Seeds"),
-                               model_formula = c(Model$Parameters$Survival$Formula,
-                                                 Model$Parameters$Growth$Formula,
-                                                 Model$Parameters$Fecundity$FlowerP$Formula,
-                                                 Model$Parameters$Fecundity$Seeds$Formula),
-                               model_type = c(Model$Parameters$Survival$Model[1],
-                                              Model$Parameters$Growth$Model[1],
-                                              Model$Parameters$Fecundity$FlowerP$Model[1],
-                                              Model$Parameters$Fecundity$Seeds$Model[1]),
-                               model_family = c(Model$Parameters$Survival$Model[2],
-                                                Model$Parameters$Growth$Model[2],
-                                                Model$Parameters$Fecundity$FlowerP$Model[2],
-                                                Model$Parameters$Fecundity$Seeds$Model[2]))
-
-FecLen <- lapply(Model$Parameters$Fecundity, FUN = function(x) x$Model[2] == 'Constant') %>%
-  unlist %>%
-  .[!.] %>%
-  length() * 2
-NotFecLen <- lapply(Model$Parameters$Fecundity, FUN = function(x) x$Model[2] == 'Constant') %>%
-  unlist %>%
-  .[. == TRUE] %>%
-  length
-
-TotalParms <- sum((length(Model$Parameters$Growth) - 2), 
-                  (length(Model$Parameters$Survival) - 2),
-                  FecLen, NotFecLen)
-ModelValues <- data.frame(ipm_code = rep("A11111", TotalParms),
-                          demographic_parameter = c(rep('Survival', 2),
-                                                    rep('Growth', 2),
-                                                    rep('Fecundity', 11 )),
-                          state_variable = rep('Size', TotalParms),
-                          parameter_type = c('Mean','Mean', # Survival
-                                             'Mean', 'Mean', 'SD', # Growth
-                                             ''),
-                                             rep('Constant', NotFecLen))
+Model <- list(Metadata = read.csv('metadata/Flat_Raw/metadata_SCL.csv',
+                                  stringsAsFactors = FALSE) %>%
+                filter(ipm_id != ''),
+              StateVar = read.csv('metadata/Flat_Raw/state_vars_SCL.csv',
+                                  stringsAsFactors = FALSE) %>%
+                filter(ipm_id != ''),
+              ModelExp = read.csv('metadata/Flat_Raw/model_exprs_SCL.csv',
+                                  stringsAsFactors = FALSE) %>%
+                filter(!is.na(ipm_id)),
+              ModelVal = read.csv('metadata/Flat_Raw/model_values_SCL.csv',
+                                  stringsAsFactors = FALSE) %>%
+                filter(!is.na(ipm_id)))
 
 dbWriteTable(conn = con,
              name = 'metadata',
-             value = Metadata,
+             value = Model$Metadata,
+             append = TRUE,
+             row.names = FALSE)
+dbWriteTable(conn = con,
+             name = 'state_variables',
+             value = Model$StateVar,
              append = TRUE,
              row.names = FALSE)
 dbWriteTable(conn = con,
              name = 'model_expression',
-             value = ModelExpressions,
+             value = Model$ModelExp,
              append = TRUE,
              row.names = FALSE)
+dbWriteTable(conn = con,
+             name = 'model_values',
+             value = Model$ModelVal,
+             append = TRUE,
+             row.names = FALSE)
+
 
   
