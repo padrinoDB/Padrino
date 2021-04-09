8e184b11697f12a57a9b96c32cb50a72556fe968
levisc8 <levisc8@gmail.com>
2021-04-09 13:10:59 +0200

    quotation marks??

--- webpage/build_webpage.R
+++ webpage/build_webpage.R
@@ -2,4 +2,4 @@
 dir.create("docs", FALSE)
 file.create("docs/.nojekyll")
 
-bookdown::render_book(input = 'webpage', output_dir = '../docs')
+bookdown::render_book(input = "webpage", output_dir = "../docs")
